1b7f62b8dbc63161a03e19fe905616a125de0a42
riggedCoinflip <tikalker@gmail.com>
2020-12-24 01:50:47 +0100

    rename 'latest_question_list'>'recent_polls'

--- tutorial/mysite/polls/views.py
+++ tutorial/mysite/polls/views.py
@@ -48,13 +48,13 @@ test method names that describe their function
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'  # would be called 'question_list' by default
+    context_object_name = 'recent_polls'
 
     def get_queryset(self):
-        '''
+        """
         Return the last five published questions (not including those set to be
         published in the future).
-        '''
+        """
         return Question.objects.filter(
             pub_date__lte=timezone.now()
         ).order_by('-pub_date')[:5]
