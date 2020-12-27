36056cdb0621ca7775bc259e88772e8236c6f8cc
riggedCoinflip <tikalker@gmail.com>
2020-12-27 01:49:23 +0100

    create form for user to create new question

--- tutorial/mysite/polls/views.py
+++ tutorial/mysite/polls/views.py
@@ -5,6 +5,7 @@ from django.views import generic
 from django.utils import timezone
 
 from .models import Choice, Question
+from .forms import QuestionForm, QuestionInlineFormSet
 
 '''
 Ideas for more tests¶
@@ -61,6 +62,37 @@ class WithSidebar:
         return context
 
 
+class IndexView(WithSidebar, generic.CreateView):
+    template_name = 'polls/index.html'
+    model = Question
+    form_class = QuestionForm
+    success_url = None
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        if self.request.POST:
+            context['question_form'] = QuestionForm(self.request.POST)
+            context['choices_form_set'] = QuestionInlineFormSet(self.request.POST)
+        else:
+            context['question_form'] = QuestionForm()
+            context['choices_form_set'] = QuestionInlineFormSet()
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        choices = context['choices_form_set']
+        if choices.is_valid() and form.is_valid():
+            question_instance = form.save()
+            for choice in choices:
+                choice = choice.instance
+                #get id of the newly created question.
+                choice.question_id = question_instance.id
+                choice.save()
+            return HttpResponseRedirect(f'{question_instance.id}')
+        else:
+            return self.render_to_response(self.get_context_data(form=form))
+
+
 class DetailView(WithSidebar, generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
@@ -70,10 +102,6 @@ class DetailView(WithSidebar, generic.DetailView):
         Excludes any questions that aren't published yet.
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
-
-
-class IndexView(WithSidebar, generic.ListView):
-    template_name = 'polls/index.html'
 
 
 class ResultsView(WithSidebar, generic.DetailView):
