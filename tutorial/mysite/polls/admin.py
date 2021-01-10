6142ff99a3cc04b55d0721b27b82806aa5fde515
riggedCoinflip <tikalker@gmail.com>
2021-01-10 19:49:51 +0100

    lint

--- tutorial/mysite/polls/admin.py
+++ tutorial/mysite/polls/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+
 # Register your models here.
 
 class ChoiceInline(admin.TabularInline):
@@ -10,9 +11,10 @@ class ChoiceInline(admin.TabularInline):
     max_num = 20
     extra = 1
 
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['question_text']}),
+        (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
@@ -20,5 +22,6 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
     search_fields = ['question_text']
+
 
 admin.site.register(Question, QuestionAdmin)
