556a1423b5a9d2d6ba54de943cb3142b9e63fac7
riggedCoinflip <tikalker@gmail.com>
2021-01-14 02:58:29 +0100

    setup database environment for github workflows

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+import os
 import json
 from pathlib import Path
 
@@ -92,6 +93,20 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+# used for github actions
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
