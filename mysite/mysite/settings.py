a7e7643e0e9c5805a89a00247c4ebd3f12b54574
riggedCoinflip <tikalker@gmail.com>
2021-01-14 03:51:44 +0100

    stop loading secret_key from untracked file DO NOT USE IN PROD

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secrets['django']['secret']
+SECRET_KEY = '2x$6a^ai+)@zp+sbypq2i_qjyh*6exi+mnb*8*d+llubwaciq4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
