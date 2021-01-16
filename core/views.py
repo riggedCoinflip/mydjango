bbb6c3f398b8802abb0faac54d6be49dbf57c5a3
riggedCoinflip <tikalker@gmail.com>
2021-01-16 15:43:07 +0100

    fix SignupView redirect after form submit

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 from django.views import generic
 
 
@@ -9,6 +10,7 @@ class SignupView(generic.CreateView):
     template_name = 'registration/signup.html'
     model = User
     form_class = UserCreationForm
+    success_url = reverse_lazy('polls:index')
 
 class ImpressumView(generic.TemplateView):
     template_name = 'legal/impressum.html'
