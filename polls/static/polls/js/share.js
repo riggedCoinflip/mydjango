0e81f98387be7155ca04a0f82b9858d2be0a7efb
riggedCoinflip <tikalker@gmail.com>
2021-02-01 14:29:57 +0100

    create new button share (not functional atm)

---
+++ polls/static/polls/js/share.js
@@ -0,0 +1,14 @@
+function copyToClipboard() {
+  /* Get the text field */
+  let copyText = document.getElementById("copyToClipboard");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
