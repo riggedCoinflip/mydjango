3ca2a4536378e9c6a334147e5225960fc90d9850
riggedCoinflip <tikalker@gmail.com>
2021-02-27 06:24:06 +0100

    fix validators - expects an iterable. use null values

--- users/models.py
+++ users/models.py
@@ -6,8 +6,8 @@ from django.core.validators import MaxLengthValidator
 class User(AbstractUser):
     is_validated = models.BooleanField(default=False)
     email = models.EmailField('email address', blank=False, unique=True)
-    about_text = models.TextField(max_length=1000, blank=False, default='', validators=MaxLengthValidator(1000))
-    github_name = models.CharField(max_length=100, validators=MaxLengthValidator(100))
+    about_text = models.TextField(max_length=1000, null=True, blank=True, validators=(MaxLengthValidator(1000),))
+    github_name = models.CharField(max_length=100, null=True, blank=True, validators=(MaxLengthValidator(100),))
     avatar = models.ImageField(blank=True, null=True)
 
     def __str__(self):
